68004fe206c8da2546ac2018aaa7acddab4687e6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-22 06:00:01 -0500

    try postcss-purgecss

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ["./index.html", "src/**/*.js"], // Be careful not to include the `node_modules` folder in your globs.
-  css: ["./styles/style.css"],
-  safelist: []
-}
