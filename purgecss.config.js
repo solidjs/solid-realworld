e51af9eed0c18fae3ad9168cd4cfb9842f947221
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-22 04:33:08 -0500

    Create purgecss.config.js

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["publix/**/*.html", "src/**/*.js"], // Be careful not to include the `node_modules` folder in your globs.
+  css: ["./styles/style.css"],
+  whitelist: [],
+}
