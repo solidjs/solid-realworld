e779a15f0a51516edff674c70af73d818d14cc36
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-22 05:47:53 -0500

    temporary: trying with parcel

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["publix/**/*.html", "src/**/*.js"], // Be careful not to include the `node_modules` folder in your globs.
+  content: ["./index.html", "src/**/*.js"], // Be careful not to include the `node_modules` folder in your globs.
   css: ["./styles/style.css"],
-  whitelist: [],
+  safelist: []
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import { render } from 'solid-js/dom';
 import App from './App';
 import { Provider } from './store';
 
-// import main css file
-import "../styles/styles.css";
-
 render(() => (
   <Provider>
     <App />
